650b606cc4e2882287def399e56e00c16fec7c01
Maciej Baba <maciek2baba2@gmail.com>
2024-12-01 21:06:02 +0100

    finish project

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
 with open('books/frankenstein.txt') as f:
     file_contents = f.read()
 
-print(file_contents)
+print('--- Begin report of books/frankenstein.txt ---')
+
+words = file_contents.split()
+word_count = len(words)
+
+print(f"{word_count} words found in the document")
+
+letter_count = {}
+
+for word in words:
+    letters = list(word)
+
+    # remove special characters
+    letters = [letter.lower() for letter in letters if letter.isalpha()]
+
+    for letter in letters:
+        if letter_count.get(letter):
+            letter_count[letter] += 1
+        else:
+            letter_count[letter] = 1
+
+for key, value in letter_count.items():
+    print(f"The '{key}' character was found {value} times")
+
+print('--- End report ---')
